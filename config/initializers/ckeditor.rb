e156d7ba144e278f0a171e12e747721c754dc875
Vitalii Kadylinskij <vitalijkadylinskij@gmail.com>
2026-01-27 20:18:27 +0300

    ref ckeditor file

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,7 +1,9 @@
 # config/initializers/ckeditor.rb
 require 'ckeditor/orm/active_record'
 
-# Регистрируем кастомные модели, чтобы Rails не пытался их автозагрузить
-Dir[Rails.root.join('app/models/ckeditor/*.rb')].each do |file|
-  require file
+Rails.application.config.to_prepare do
+  # любые кастомные настройки CKEditor здесь
+  Ckeditor.setup do |config|
+    # config settings, если нужно
+  end
 end
