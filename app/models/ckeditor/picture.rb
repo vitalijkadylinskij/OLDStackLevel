6b02a8ab63f7082d4a7f8318489b03ca5afdaf77
Vitalii Kadylinskij <vitalijkadylinskij@gmail.com>
2026-01-27 15:31:42 +0300

    asdasdasd

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class Ckeditor::Picture < Ckeditor::Asset
-  mount_uploader :data, CkeditorPictureUploader, mount_on: :data_file_name
+#class Ckeditor::Picture < Ckeditor::Asset
+  #mount_uploader :data, CkeditorPictureUploader, mount_on: :data_file_name
 
-  def url_content
-    url(:content)
-  end
-end
+  #def url_content
+    #url(:content)
+  #end
+#end
