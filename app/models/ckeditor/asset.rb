ec9abfe4fc294e0273c1f78cf3d4859e580d3741
Vitalii Kadylinskij <vitalijkadylinskij@gmail.com>
2026-01-27 17:14:28 +0300

    aaaaaaaas

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,9 +1,3 @@
-# frozen_string_literal: true
-
-class Ckeditor::Asset < Ckeditor::ORM::ActiveRecord::Asset
-  include Ckeditor::Orm::ActiveRecord::AssetBase
-
-  delegate :url, :current_path, :content_type, to: :data
-
-  validates :data, presence: true
+class Ckeditor::Asset < Ckeditor::ApplicationRecord
+  include Ckeditor::Orm::ActiveRecord::Asset
 end
