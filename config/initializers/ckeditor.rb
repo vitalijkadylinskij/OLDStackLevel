348eee0d06195991ca7bbcbc9f5744261c267871
Vitalii Kadylinskij <vitalijkadylinskij@gmail.com>
2026-01-27 15:25:16 +0300

    qqe

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 # Use this hook to configure ckeditor
-#Ckeditor.setup do |config|
+#!Ckeditor.setup do |config|
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default), :mongo_mapper and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  #require 'ckeditor/orm/active_record'
+ #! require 'ckeditor/orm/active_record'
 
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
@@ -51,4 +51,4 @@
   # Used when CKEditor CDN enabled
   # By default: "ckeditor/config.js"
   # config.js_config_url = 'ckeditor/config.js'
-#end
+#!end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
+#  mount Ckeditor::Engine => '/ckeditor'
 
   devise_for :users,
              controllers: { omniauth_callbacks: 'sessions/omniauth_callbacks' }
