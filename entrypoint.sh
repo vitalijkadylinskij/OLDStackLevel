0fd89b86936a159add78c66ae1f39eda276ef2a5
Vitalii Kadylinskij <vitalijkadylinskij@gmail.com>
2026-01-28 03:18:44 +0300

    bbb

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Creating database (if not exists)..."
-
-echo "Running migrations..."
-bundle exec rails db:migrate
-
 echo "Starting Rails on port $PORT"
 exec bundle exec rails server -b 0.0.0.0 -p $PORT
